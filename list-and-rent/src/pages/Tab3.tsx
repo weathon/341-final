28384eaf5ac23a6fce7680f583e962216e47a693
Wayne <wguo6358@gmail.com>
2023-11-30 02:52:32 +0000

    -

--- list-and-rent/src/pages/Tab3.tsx
+++ list-and-rent/src/pages/Tab3.tsx
@@ -2,22 +2,39 @@ import { IonButton, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonList, 
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 import { images } from 'ionicons/icons';
+import { useState } from 'react';
 
 const Tab3: React.FC = () => {
   const catOptions = ["Books", "Sport Gears", "Electronics"];
+  const [image, setImage] = useState("");
   return (
     <IonPage>
+      <input type='file' hidden id="file" onChange={(e) => {
+        let reader = new FileReader();
+        let res = reader.readAsDataURL(e.target.files[0]);
+        reader.onload = () => {
+          console.log(reader.result)
+          //@ts-ignore
+          setImage(reader.result)
+        }
+      }}></input>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Post a new Listing</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <div className='m-5 bg-slate-200 h-2/6 rounded-lg grid text-center place-content-center'>
+        <div onClick={() => {
+          //@ts-ignore
+          document.getElementById("file").click()
+        }} className='m-5 bg-slate-200 h-2/6 rounded-lg grid text-center place-content-center'>
           {/* now i do not need text-center wtf oh i have child in div */}
           <div>
-            <IonIcon icon={images} className="text-4xl"></IonIcon>
-            <p>Add a photo here</p>
+            {
+              !image ?<><IonIcon icon={images} className="text-4xl"></IonIcon>
+                <p>Add a photo here</p></>
+              :<img src={image}></img>
+            }
           </div>
         </div>
         <IonList>
@@ -42,7 +59,12 @@ const Tab3: React.FC = () => {
           <IonItem>
             <IonInput type="number" label="Price" placeholder='Enter price'></IonInput>
           </IonItem>
-            <IonButton className="m-3" expand='block'>Submit</IonButton>
+          <IonButton className="m-3" expand='block' onClick={()=>{
+            setTimeout(()=>{
+              alert("Submited!")
+              window.location.href = window.location.href
+            }, 500)
+          }}>Submit</IonButton>
 
         </IonList>
       </IonContent>
